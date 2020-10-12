fb975ad9b162086cac8f6885824cca1ef82dada0
Valencinus Aten <valenaten22@gmail.com>
2020-10-12 14:43:45 +0700

    Tugas 19 js
    Tugas ini saya sampaikan ke tim Niomic

---
+++ tugas19.js
@@ -0,0 +1,19 @@
+function perulangan(){
+  var ulang = "Semangat...";
+  console.log(ulang.repeat(5));
+}
+perulangan()
+
+function bintang(){
+var ulang = "*";
+  console.log(ulang.repeat(5));
+    console.log(ulang.repeat(15));
+      console.log(ulang.repeat(30));
+        console.log(ulang.repeat(45));
+          console.log(ulang.repeat(60));
+            console.log(ulang.repeat(45));
+              console.log(ulang.repeat(30));
+                console.log(ulang.repeat(15));
+                  console.log(ulang.repeat(5));
+}
+bintang()
